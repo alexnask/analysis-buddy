6e8175aa0c6c5f85b05eb8fec39ec106a76bd3bd
Alexandros Naskos <alex_naskos@hotmail.com>
2020-07-01 02:19:54 +0300

    Better analysis, bad commit message

--- src/main.zig
+++ src/main.zig
@@ -9,28 +9,28 @@ pub fn getFieldAccessType(
     arena: *std.heap.ArenaAllocator,
     handle: *DocumentStore.Handle,
     tokenizer: *std.zig.Tokenizer,
-) !?analysis.TypeWithHandle {
+    bound_type_params: *analysis.BoundTypeParams,
+) !?analysis.DeclWithHandle {
     var current_type = analysis.TypeWithHandle.typeVal(.{
         .node = &handle.tree.root_node.base,
         .handle = handle,
     });
 
-    var bound_type_params = analysis.BoundTypeParams.init(&arena.allocator);
     while (true) {
         const tok = tokenizer.next();
         switch (tok.id) {
-            .Eof => return try analysis.resolveFieldAccessLhsType(store, arena, current_type, &bound_type_params),
             .Identifier => {
                 if (try analysis.lookupSymbolGlobal(store, arena, current_type.handle, tokenizer.buffer[tok.loc.start..tok.loc.end], 0)) |child| {
-                    current_type = (try child.resolveType(store, arena, &bound_type_params)) orelse return null;
+                    current_type = (try child.resolveType(store, arena, bound_type_params)) orelse return null;
                 } else return null;
             },
             .Period => {
                 const after_period = tokenizer.next();
                 switch (after_period.id) {
-                    .Eof => return try analysis.resolveFieldAccessLhsType(store, arena, current_type, &bound_type_params),
                     .Identifier => {
-                        current_type = try analysis.resolveFieldAccessLhsType(store, arena, current_type, &bound_type_params);
+                        current_type = try analysis.resolveFieldAccessLhsType(store, arena, current_type, bound_type_params);
+
+                        const last_ident = tokenizer.buffer.len == after_period.loc.end;
 
                         var current_type_node = switch (current_type.type.data) {
                             .other => |n| n,
@@ -38,7 +38,7 @@ pub fn getFieldAccessType(
                         };
 
                         if (current_type_node.cast(ast.Node.FnProto)) |func| {
-                            if (try analysis.resolveReturnType(store, arena, func, current_type.handle, &bound_type_params)) |ret| {
+                            if (try analysis.resolveReturnType(store, arena, func, current_type.handle, bound_type_params)) |ret| {
                                 current_type = ret;
                                 current_type_node = switch (current_type.type.data) {
                                     .other => |n| n,
@@ -54,7 +54,11 @@ pub fn getFieldAccessType(
                             tokenizer.buffer[after_period.loc.start..after_period.loc.end],
                             !current_type.type.is_type_val,
                         )) |child| {
-                            current_type = (try child.resolveType(store, arena, &bound_type_params)) orelse return null;
+                            if (last_ident) {
+                                return child;
+                            }
+
+                            current_type = (try child.resolveType(store, arena, bound_type_params)) orelse return null;
                         } else return null;
                     },
                     else => {
@@ -68,7 +72,9 @@ pub fn getFieldAccessType(
         }
     }
 
-    return try analysis.resolveFieldAccessLhsType(store, arena, current_type, &bound_type_params);
+    return null;
+
+    // return try analysis.resolveFieldAccessLhsType(store, arena, current_type, &bound_type_params);
 }
 
 pub fn main() anyerror!void {
@@ -100,6 +106,11 @@ pub fn main() anyerror!void {
     var timer = try std.time.Timer.start();
 
     while (true) {
+        defer {
+            arena.deinit();
+            arena.state.buffer_list = .{};
+        }
+
         const line = reader.readUntilDelimiterAlloc(gpa, '\n', std.math.maxInt(usize)) catch |e| switch (e) {
             error.EndOfStream => break,
             else => return e,
@@ -111,31 +122,56 @@ pub fn main() anyerror!void {
             std.debug.print("Took {} ns to complete request.\n", .{timer.lap()});
         }
 
-        var tokenizer = std.zig.Tokenizer.init(line);
-        if (try getFieldAccessType(&doc_store, &arena, root_handle, &tokenizer)) |result| {
+        var bound_type_params = analysis.BoundTypeParams.init(&arena.allocator);
+        var tokenizer = std.zig.Tokenizer.init(line[0..line.len-1]);
+        if (try getFieldAccessType(&doc_store, &arena, root_handle, &tokenizer, &bound_type_params)) |result| {
             if (result.handle != root_handle) {
-                const result_uri = result.handle.uri()[lib_uri.len..];
-                switch (result.type.data) {
-                    .other => |n| {
-                        const start_tok = if (analysis.getDocCommentNode(result.handle.tree, n)) |doc_comment|
+                switch (result.decl.*) {
+                    .ast_node => |n| {
+                        var handle = result.handle;
+                        var node = n;
+                        if (try analysis.resolveVarDeclAlias(&doc_store, &arena, .{ .node = node, .handle = result.handle })) |alias_result| {
+                            switch (alias_result.decl.*) {
+                                .ast_node => |inner| {
+                                    handle = alias_result.handle;
+                                    node = inner;
+                                },
+                                else => {},
+                            }
+                        } else if (node.cast(ast.Node.VarDecl)) |vdecl| try_import_resolve: {
+                            if (vdecl.init_node) |init| {
+                                if (init.cast(ast.Node.BuiltinCall)) |builtin| {
+                                    if (std.mem.eql(u8, handle.tree.tokenSlice(builtin.builtin_token), "@import") and builtin.params_len == 1) {
+                                        const import_type = (try result.resolveType(&doc_store, &arena, &bound_type_params)) orelse break :try_import_resolve;
+                                        switch (import_type.type.data) {
+                                            .other => |resolved_node| {
+                                                handle = import_type.handle;
+                                                node = resolved_node;
+                                            },
+                                            else => {},
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        const start_tok = if (analysis.getDocCommentNode(handle.tree, node)) |doc_comment|
                             doc_comment.first_line
                         else
-                            n.firstToken();
+                            node.firstToken();
 
-                        const start_loc = result.handle.tree.tokenLocation(0, start_tok);
-                        const end_loc = result.handle.tree.tokenLocation(0, n.lastToken());
+                        const result_uri = handle.uri()[lib_uri.len..];
+
+                        const start_loc = handle.tree.tokenLocation(0, start_tok);
+                        const end_loc = handle.tree.tokenLocation(0, node.lastToken());
                         const github_uri = try std.fmt.allocPrint(&arena.allocator, "https://github.com/ziglang/zig/blob/master/lib{}#L{}-L{}\n", .{ result_uri, start_loc.line + 1, end_loc.line + 1 });
                         try std.io.getStdOut().writeAll(github_uri);
+                        continue;
                     },
                     else => {},
                 }
             }
-        } else {
-            std.debug.print("No match found\n", .{});
-            try std.io.getStdOut().writeAll("\n");
         }
-
-        arena.deinit();
-        arena.state.buffer_list = .{};
+        std.debug.print("No match found\n", .{});
+        try std.io.getStdOut().writeAll("\n");
     }
 }
